628f53f4c5beb174c447e1121814c2bc4f2c9eea
sheng <sheng@iastate.edu>
2017-06-19 21:06:50 -0500

    Reseting

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,12 @@
+package org.openshift;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author macli
+ */
+public class JaxrsConfig extends Application {
+    
+}
